0f028e819da2896ae6b1c58e5d96334787e49fd6
NoahMic <doyun5854@gmail.com>
2022-09-09 14:18:41 +0900

    cors 수장

--- main.py
+++ main.py
@@ -6,7 +6,23 @@ from sqlalchemy.orm import Session
 from sqlalchemy.sql import func
 from models import DataModel
 from db import SessionLocal
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+origins = [
+    "http://localhost:8080",
+    "http://localhost:3000",
+    "http://localhost"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def get_db():
    db = SessionLocal()
