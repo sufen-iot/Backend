9f504bbc2ca50d7bac9e79f6540ebc75137a3614
Doyun Kim <82945118+NoahMic@users.noreply.github.com>
2022-09-15 01:07:13 +0900

    주석 추가

--- schema.py
+++ schema.py
@@ -1,11 +1,13 @@
 from pydantic import BaseModel
 
+# 하드웨어에서 보내는 데이터 형태
 class RequestModel(BaseModel):
     heading: int
     latitude: float
     longitude:float
     img: str
 
+# 하드웨어 보내는 하드웨어 정보 데이터 형태
 class RequestHardwareModel(BaseModel):
     cpu: str
     ram: str
@@ -16,6 +18,7 @@ class RequestHardwareModel(BaseModel):
     class Config:
         orm_mode=True
         
+# 프론트엔드로 보내주기 위해 가공한 데이터 형태
 class ResponseDataModel(BaseModel):
     id : str
     time : str
@@ -26,6 +29,7 @@ class ResponseDataModel(BaseModel):
     heading : int
         
 
+# 프론트엔드로 보내는 데이터 형태
 class ResponseModel(BaseModel):
     data: ResponseDataModel
     status: str = "success"
@@ -33,6 +37,7 @@ class ResponseModel(BaseModel):
     class Config:
         orm_mode=True
         
+# 프론트엔드에서 스테이터스를 수정하기 위한 요청을 보낼 때 받는 데이터 형태
 class PutModel(BaseModel):
     id: str
     status: bool
